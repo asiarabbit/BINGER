0d74c1e8434aece791c0def4dcbb3d1058e0c773
asiarabbit <asia.rabbit@163.com>
2018-01-03 21:23:56 +0800

    git push test for dev

--- build/go.sh
+++ build/go.sh
@@ -3,25 +3,33 @@
 file=gSIM.root
 isDCArrR=1
 sim(){
-./sim 1 100 SIM0.root
-./sim 1 100 SIM1.root
-./sim 1 100 SIM2.root
-./sim 1 100 SIM3.root
-./sim 1 100 SIM4.root
-./sim 1 100 SIM5.root
-./sim 1 100 SIM6.root
-./sim 1 100 SIM7.root
+echo "To genearte simualtion data"
+sleep 3
+./sim 1 100 SIM0.root &
+sleep 2; ./sim 1 100 SIM0.root &
+sleep 4; ./sim 1 100 SIM2.root &
+sleep 6; ./sim 1 100 SIM3.root &
+wait
+./sim 1 100 SIM4.root &
+sleep 2; ./sim 1 100 SIM5.root &
+sleep 4; ./sim 1 100 SIM6.root &
+sleep 6; ./sim 1 100 SIM7.root &
+wait
 }
 # particle tracking
 pre(){
-./pre -d5 SIM0.root
-./pre -d5 SIM1.root
-./pre -d5 SIM2.root
-./pre -d5 SIM3.root
-./pre -d5 SIM4.root
-./pre -d5 SIM5.root
-./pre -d5 SIM6.root
-./pre -d5 SIM7.root
+echo "To analyze data"
+sleep 3
+./pre -d5 SIM0.root &
+sleep 5; ./pre -d5 SIM1.root &
+sleep 4; ./pre -d5 SIM2.root &
+sleep 6; ./pre -d5 SIM3.root &
+wait
+./pre -d5 SIM4.root &
+sleep 2; ./pre -d5 SIM5.root &
+sleep 4; ./pre -d5 SIM6.root &
+sleep 6; ./pre -d5 SIM7.root &
+wait
 if [ -f $file ]; then
     rm $file
 fi
@@ -36,11 +44,14 @@ if [ -f 'SIM0.root' ]; then
 fi
 
 sim
+wait;
+echo "sim data have been generated"
+sleep 3
 pre
 ./t0 $file $isDCArrR
-for i in $(seq 0 3); do
-    ./ass $file $isDCArrR $i
-    ./str $file $isDCArrR $i
-    pre
-done
-echo "Accomplished"
+#for i in $(seq 0 3); do
+#    ./ass $file $isDCArrR $i
+#    ./str $file $isDCArrR $i
+#    pre
+#done
+#echo "Accomplished"
