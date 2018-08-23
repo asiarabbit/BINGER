00baff6380cce193d9ae486a75899f8565f47508
asiarabbit <asia.rabbit@163.com>
2018-08-23 15:38:14 +0800

    Pion

--- build/tmp.C
+++ build/tmp.C
@@ -6,9 +6,18 @@ void tmp(){
 	treeTrack->AddFriend(shoot);
 	treeTrack->AddFriend(vme);
 
-
+	// PID
 	treeTrack->Draw("dE0:tof1>>(500, 133., 137., 500, -0.5, 4.5)", "", "col");
 	treeTrack->Draw("dE1:dE0>>(500, -1., 5., 500, -1., 5.)", "", "col");
+	treeTrack->Draw("aoz>>(500, 1.4, 2.7)", "");
+	// overall PID
+	treeTrack->Draw("dE1:aoz>>(500, 1.4, 3., 500, 0., 4.5)", "", "col")
+	treeTrack->Draw("dE1:aoz>>(500, 1.4, 3., 500, 0., 4.5)", "dsca11<=0&&dsca10<=1", "col")
+	treeTrack->Draw("dE1:aoz>>(500, 1.4, 3., 500, 0., 4.5)", "!(dsca11<=0&&dsca10<=1)", "col")
 
-	treeTrack->Draw("aoz>>(500, 1.4, 2.7)", "CUTG0");
+	// track count
+	double(treeTrack->GetEntries("ntr==0&&A04"))/treeTrack->GetEntries("A04")
+	double(treeTrack->GetEntries("ntrLs[3][0]==0&&A04"))/treeTrack->GetEntries("A04")
+
+
 }
